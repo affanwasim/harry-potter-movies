8ab1a3cb9099380e387b8566f9f0a4045b4f1a70
Tayyab Wasim <affanwasim@gmail.com>
2024-03-17 11:03:23 -0500

    updates

--- src/app/movies.service.ts
+++ src/app/movies.service.ts
@@ -2,13 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Movie } from './model/movie';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
-  moviesURL:string = 'http://localhost:4200/movies'
-  constructor(private http:HttpClient) { }
+  moviesURL:string;
+  constructor(private router:Router,private http:HttpClient) { 
+    this.moviesURL = router.url;
+  }
 
   getMovie(id:string):Observable<Movie> {
     return this.http.get<Movie>(this.moviesURL+'/'+id);
